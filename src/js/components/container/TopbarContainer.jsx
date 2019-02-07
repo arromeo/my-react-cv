e00ba876abe7cbd14b4253c4c41358d7ab022c7a
Adam Romeo <adam.r.romeo@gmail.com>
2019-02-06 20:28:34 -0800

    Another attempt

--- src/js/components/container/TopbarContainer.jsx
+++ src/js/components/container/TopbarContainer.jsx
@@ -4,7 +4,7 @@ import Contact from '../presentational/Contact.jsx';
 
 import styles from './Topbar.module.scss';
 
-const TopbarContainer = ({name, title, skills}) => (
+const TopbarContainer = ({name, title}) => (
   <div className={styles.topbar}>
     <div className={styles.header}>
       <Nameplate name={name} title={title} />
